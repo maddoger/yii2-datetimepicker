42d10d87a09cfee9ced79b84e530449b2c1c7b3c
Vitaliy Syrchikov <maddoger@gmail.com>
2014-11-09 20:48:59 +0400

    Bower assets

--- DateTimePickerAsset.php
+++ DateTimePickerAsset.php
@@ -7,22 +7,16 @@ use yii\web\AssetBundle;
 class DateTimePickerAsset extends AssetBundle
 {
 	public $css = [
-		'bootstrap-datetimepicker.min.css',
+		'@bower/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css',
 	];
 
 	public $js = [
-        'moment-with-locales.js',
-		'bootstrap-datetimepicker.min.js',
+        '@bower/moment/min/moment-with-locales-min.js',
+		'@bower/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js',
 	];
 
 	public $depends = [
 		'yii\bootstrap\BootstrapAsset',
 		'yii\bootstrap\BootstrapPluginAsset',
 	];
-
-    public function init()
-    {
-        parent::init();
-        $this->sourcePath = __DIR__.'/assets';
-    }
 }
