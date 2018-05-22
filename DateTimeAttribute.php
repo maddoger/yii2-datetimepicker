bd9aba8dd553333b6e1fa01f12332fa8e6491a1b
Vitaliy Syrchikov <vitaliy.syrchikov@castle.co>
2018-05-22 13:00:50 +0400

    Backward compatibility

---
+++ DateTimeAttribute.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @copyright Copyright (c) 2014 Vitaliy Syrchikov
+ * @link http://syrchikov.name
+ */
+
+namespace maddoger\widgets;
+
+class DateTimeAttribute extends \maddoger\behaviors\DateTimeAttribute {
+}
